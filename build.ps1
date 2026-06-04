b3c683519aa19149b6aaa07dae68228c0585849f
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2026-06-04 16:52:47 +0530

    chore: Updated `build.ps1` to remove unnecessary file and folder.
    This commit reduces the file size of the build significantly by excluding unwanted files that are not required for build.
    
    > Also enables `Uninstall` button when right clicking the software in start

--- build.ps1
+++ build.ps1
@@ -93,7 +93,7 @@ try {
         --build-version $version `
         --company "Pooja ITR Center" `
         --copyright "Copyright (C) 2025 Pooja ITR Center" `
-        --exclude "release, icons, requirements.txt, README.md, certs, test, .venv" `
+        --exclude ".git, build.ps1, release, icons, Sola-cache, README.md, certs, test, .gitignore, .vscode, .venv, Sola.aip, sola-updater.aip, updates.txt, requirements.txt, README.md, poetry.lock" `
         --clear-cache --compile-app --compile-packages --cleanup-app --cleanup-packages --cleanup-app --module-name .\main.py
 
     if ($LASTEXITCODE -eq 0) {
